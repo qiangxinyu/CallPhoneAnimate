6c34eead89cdc09931ab47c6dfe35c3a77ba9edb
apple <apple@appledeiMac.local>
2015-07-24 20:50:58 +0800

    Initial Commit

---
+++ callPhone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  callPhone
+//
+//  Created by apple on 15/7/24.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ callPhone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  callPhone
+//
+//  Created by apple on 15/7/24.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
